1ff4f5658337d8711f98211359fd938d787e1c29
irtesham-hossain <irtesham258@gmail.com>
2025-11-27 00:57:37 +0600

    irte

---
+++ Cement_Factory/src/main/java/cse213/cement_factory/Maruf_2130208/test_controller.java
@@ -0,0 +1,7 @@
+package cse213.cement_factory.Maruf_2130208;
+
+public class test_controller
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ Cement_Factory/src/main/java/cse213/cement_factory/Sazia_2222521/test_controller.java
@@ -0,0 +1,7 @@
+package cse213.cement_factory.Sazia_2222521;
+
+public class test_controller
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
