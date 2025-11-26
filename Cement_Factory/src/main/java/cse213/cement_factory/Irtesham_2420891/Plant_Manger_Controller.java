81063398ed45b5430e8cd914218c2271d02da317
irtesham-hossain <irtesham258@gmail.com>
2025-11-27 00:30:32 +0600

    irte

---
+++ Cement_Factory/src/main/java/cse213/cement_factory/Irtesham_2420891/Plant_Manger_Controller.java
@@ -0,0 +1,7 @@
+package cse213.cement_factory.Irtesham_2420891;
+
+public class Plant_Manger_Controller
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ Cement_Factory/src/main/java/cse213/cement_factory/Maliha_2420913/Dealer_Controller.java
@@ -0,0 +1,7 @@
+package cse213.cement_factory.Maliha_2420913;
+
+public class Dealer_Controller
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
