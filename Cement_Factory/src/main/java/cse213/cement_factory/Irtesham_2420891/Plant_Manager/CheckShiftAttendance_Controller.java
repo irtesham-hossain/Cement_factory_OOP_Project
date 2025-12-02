4b2223bec19f9fd9b0c41e5d87868b1d9dc77281
irtesham-hossain <irtesham258@gmail.com>
2025-12-02 10:41:55 +0600

    irte

--- Cement_Factory/src/main/java/cse213/cement_factory/Irtesham_2420891/Plant_Manager/CheckShiftAttendance_Controller.java
+++ Cement_Factory/src/main/java/cse213/cement_factory/Irtesham_2420891/Plant_Manager/CheckShiftAttendance_Controller.java
@@ -3,5 +3,37 @@ package cse213.cement_factory.Irtesham_2420891.Plant_Manager;
 public class CheckShiftAttendance_Controller
 {
     @javafx.fxml.FXML
+    private TableColumn dateCol;
+    @javafx.fxml.FXML
+    private Button addTableButton;
+    @javafx.fxml.FXML
+    private Button claerButton;
+    @javafx.fxml.FXML
+    private Button backButton;
+    @javafx.fxml.FXML
+    private TableColumn WorkerAbsentCol;
+    @javafx.fxml.FXML
+    private TableColumn ShiftNumberCol;
+    @javafx.fxml.FXML
+    private TableColumn WorkerPresentCol;
+    @javafx.fxml.FXML
+    private DatePicker attendance_date;
+    @javafx.fxml.FXML
+    private ComboBox shiftNumberCombo;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void clearONF(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void addToTableONF(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BackONF(ActionEvent actionEvent) {
+    }
+}
