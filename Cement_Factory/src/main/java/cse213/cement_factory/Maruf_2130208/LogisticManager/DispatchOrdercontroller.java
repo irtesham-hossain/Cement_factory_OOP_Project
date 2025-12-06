2612670299a83800add1d1540d8ebb7708c99627
marufalhasaann <marufalhasaann@gmail.com>
2025-12-06 16:47:55 +0600

    maruf

---
+++ Cement_Factory/src/main/java/cse213/cement_factory/Maruf_2130208/LogisticManager/DispatchOrdercontroller.java
@@ -0,0 +1,28 @@
+package cse213.cement_factory.Maruf_2130208.LogisticManager;
+
+public class DispatchOrdercontroller {
+    @javafx.fxml.FXML
+    private TableColumn customerNameTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn orderIDTableColumn;
+    @javafx.fxml.FXML
+    private TableView pendingOrdersTableView;
+    @javafx.fxml.FXML
+    private TableColumn destinationTableColumn;
+    @javafx.fxml.FXML
+    private RadioButton rejectRadioButton;
+    @javafx.fxml.FXML
+    private RadioButton approveRadioButton;
+    @javafx.fxml.FXML
+    private TableColumn dateTableColumn;
+    @javafx.fxml.FXML
+    private ToggleGroup actionToggleGroup;
+
+    @javafx.fxml.FXML
+    public void updateStatusButtonOnClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void goBackButtonOnClick(ActionEvent actionEvent) {
+    }
+}
---
+++ Cement_Factory/src/main/java/cse213/cement_factory/Maruf_2130208/LogisticManager/ScheduleDeliverycontroller.java
@@ -0,0 +1,27 @@
+package cse213.cement_factory.Maruf_2130208.LogisticManager;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+
+public class ScheduleDeliverycontroller {
+    @javafx.fxml.FXML
+    private TableColumn customerNameTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn loactionTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn orderIdTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn deliverypriorityTableColumn;
+
+    @javafx.fxml.FXML
+    public void prioritylevelhandler(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void schedulefordeliveryonclick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void orderidhandler(ActionEvent actionEvent) {
+    }
+}
